d4ae467b8b056a19551298b7326b5358daa3bb01
philipwoite <philip.woite@gmail.com>
2015-06-21 21:11:55 +0200

    My sixth proposed solution - tidied up fifth proposal

--- cachematrix.R
+++ cachematrix.R
@@ -1,68 +1,45 @@
-#<<-## Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what your
 ## functions do
+
+# makeCacheMatrix and cacheSolve work together (with cacheSolve calling makeCacheMatrix) to solve and "cache" the inverse of an invertable square matrix
 
 ## Write a short comment describing this function
 ## 2015-06-21 Philip Woite
-## This function {makeCacheMatrix} will calculate the inverse of the input matrix, and then cache it in memory
 
-#environment(MakeCacheMatrix) <- myNewEnvironment
+# This function {makeCacheMatrix} will calculate the inverse of the input matrix, and then cache it in memory
 
 makeCacheMatrix <- function(x = matrix()) {
-## 14:45
-#environment(makeCacheMatrix) <- donder"
-	#myne <- x  
-  if(!exists("myne", inherit = TRUE))  {
-  #if(("myne" %in% names(env))  {
-  #  myne <- myne} else {
-      
-#	setMyneNull <- function() {
-	  
-    message("myne did not yet exist")
-    myne <<- NULL}
- #}
+
+	if(!exists("myne", inherit = TRUE))  {
+		message("myne did not yet exist")
+		myne <<- NULL
+		}
   
 	workingMatrix <- x  
-#  set <- function() myne <<- NULL
 
 	goget <- function() workingMatrix
 	setinverse <- function() myne <<- solve(workingMatrix)
 	getinverse <- function() myne
-	#set <- function
 
-	#list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-
-#	setinverse
-
-	list(#setMyneNull = setMyneNull, 
-    goget = goget, setinverse = setinverse, getinverse = getinverse)
-
-
-
-	#return(inverseMatrix)
-#print(environmentName(makeCacheMatrix))
+	list(goget = goget, setinverse = setinverse, getinverse = getinverse)
 }
 
-theEnv <- environmentName(makeCacheMatrix)
 ## Write a short comment describing this function
 
+# My solution for cacheSolve starts by calling makeCacheMatrix, passing the matrix "x" to makeCacheMatrix
 cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
   
- # assign(parent.env(makeCacheMatrix)
-  makeCacheMatrix(x)	
- # print(myne <- makeCacheMatrix(x)$goget())
+	makeCacheMatrix(x)	
+
 	if(!is.null(myne)) {
 		message("getting the cached inverse")
 		myne <- makeCacheMatrix(x)$getinverse()
-	}
-    else {
-	data <- x["goget()"]
-  message("I have to calculate the inverse")
-	myne <- (makeCacheMatrix(x)$setinverse())
-#	x["makeCacheMatrix$setinverse(myne)"]
-#      x["setinverse()"]	
-  
-    }
-#inverseMatrix <- solve(workingMatrix)
+		}
+    		else {
+		data <- x["goget()"]
+  		message("I have to calculate the inverse")
+		myne <- (makeCacheMatrix(x)$setinverse())
+		}
 return(myne)
 }
