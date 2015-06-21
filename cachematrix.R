e3456b1674b05ab0745d4e6795dd207459db60e2
philipwoite <philip.woite@gmail.com>
2015-06-21 19:30:18 +0200

    My third proposed solution - Doing final testing. This is based on the makeVector and cachemean examples given in the ProgrammingAssignment2 README

--- cachematrix.R
+++ cachematrix.R
@@ -5,18 +5,24 @@
 ## 2015-06-21 Philip Woite
 ## This function {makeCacheMatrix} will calculate the inverse of the input matrix, and then cache it in memory
 
+#environment(MakeCacheMatrix) <- myNewEnvironment
+
 makeCacheMatrix <- function(x = matrix()) {
 ## 14:45
-	workingMatrix <- x  
+#environment(makeCacheMatrix) <- donder"
+	#myne <- x  
 	
 	set <- function(y) {
-	  x <<- y
-	  myne <<- NULL
-	}
+#	  x <<- y
+
+  myne <<- NULL
+}
+  
+	workingMatrix <- x  
 #  set <- function() myne <<- NULL
 
-	get <- function() workingMatrix
-	setinverse <- function() myne <<- solve(myne)
+	goget <- function() workingMatrix
+	setinverse <- function() myne <<- solve(workingMatrix)
 	getinverse <- function() myne
 	#set <- function
 
@@ -24,28 +30,37 @@ makeCacheMatrix <- function(x = matrix()) {
 
 	setinverse
 
-	list(get = get, setinverse = setinverse, getinverse = getinverse)
+	list(#set = set, 
+    goget = goget, setinverse = setinverse, getinverse = getinverse)
 
 
 
 	#return(inverseMatrix)
+#print(environmentName(makeCacheMatrix))
 }
 
-
+theEnv <- environmentName(makeCacheMatrix)
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
-	myne <- x["getinverse()"]
+
+  
+ # assign(parent.env(makeCacheMatrix)
+   makeCacheMatrix(x)	
+ # print(myne <- makeCacheMatrix(x)$goget())
 	if(!is.null(myne)) {
 		message("getting the cached inverse")
-		return(myne)
+		return(makeCacheMatrix(x)$getinverse())
 	}
-	data <- x$get()
-	myne <- solve(data, ...)
-	x["setinverse(myne)"]
-	myne
-
+    else {
+	data <- x["goget()"]
+  message("I have to calculate the inverse")
+	myne <- (makeCacheMatrix(x)$setinverse())
+#	x["makeCacheMatrix$setinverse(myne)"]
+      x["setinverse()"]	
+      myne
+    }
 #inverseMatrix <- solve(workingMatrix)
 
 }
