1e8ffd9a7df988997a14abdd46633f8307fac865
philipwoite <philip.woite@gmail.com>
2015-06-21 21:21:31 +0200

    My seventh proposed solution. I have updated my comments and messages.

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,18 @@
 ## functions do
 
 # makeCacheMatrix and cacheSolve work together (with cacheSolve calling makeCacheMatrix) to solve and "cache" the inverse of an invertable square matrix
+# I have named the inverse matrix "myne" which translated from Afrikaans to English simply means "mine", saying the inverse belongs to me. 
+
 
 ## Write a short comment describing this function
 ## 2015-06-21 Philip Woite
 
-# This function {makeCacheMatrix} will calculate the inverse of the input matrix, and then cache it in memory
+# The function "makeCacheMatrix" will calculate the inverse of the input matrix, and then cache it in memory in the list that is returned.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 	if(!exists("myne", inherit = TRUE))  {
-		message("myne did not yet exist")
+		message("The inverse matrix did not yet exist, so I will initialise it with a NULL value.")
 		myne <<- NULL
 		}
   
@@ -27,18 +29,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 # My solution for cacheSolve starts by calling makeCacheMatrix, passing the matrix "x" to makeCacheMatrix
+
 cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
   
 	makeCacheMatrix(x)	
 
 	if(!is.null(myne)) {
-		message("getting the cached inverse")
+		message("I'm getting the cached inverse")
 		myne <- makeCacheMatrix(x)$getinverse()
 		}
     		else {
 		data <- x["goget()"]
-  		message("I have to calculate the inverse")
+  		message("I have to first calculate the inverse")
 		myne <- (makeCacheMatrix(x)$setinverse())
 		}
 return(myne)
