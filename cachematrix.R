48698d98bb14f0b31979036cffca008c6169404d
philipwoite <philip.woite@gmail.com>
2015-06-21 15:10:23 +0200

    Second attempt to commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,13 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 2015-06-21 Philip Woite
+## This function {makeCacheMatrix} will calculate the inverse of the input matrix, and then cache it in memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## 14:45
+inverseMatrix <- solve(x)
+return(inverseMatrix)
 }
 
 
